da801e360fb3cd6eebe24ad0357bdf5b40d07251
Dean Svahnalam <56052788+Cyber-pgp@users.noreply.github.com>
2024-04-01 23:45:58 +0200

    Update string_indexting.py
    described String Slicing

--- string_indexting.py
+++ string_indexting.py
@@ -11,8 +11,19 @@ print([-1] # This will print 'E'
 # By using position numbers we can pick out any letters from the word.
 
 # Lab 1
-# Given the string | s = 'Python' | Use indexning to print the following output:'o''Pyth''yth''nohtyP'
+# Given the string | s = 'Python' | Use indexing to print the following output:'o''Pyth''yth''nohtyP'
 
-s = 'Python'
+s = 'Python' # index: 0,1,2,3,4,5 - -1,-2,-3,-4, -5, -6, -7
 print(s[4] # This will print 'o'
-print(s[
+
+# Slicing Strings - with slicing we can get the whole piece of string, not just a character.
+print(s[0:4] # Grabing from the start up to index 4 but not include index 4 will print 'Pyth'
+
+print(s[1:4] # This will print 'yth'
+
+# We can use negative numbers to slice from the end backward
+print(s[-3:-7] # This will print 'Pyth'
+
+print(s[-3:-6] # This print 'yth'
+
+      
