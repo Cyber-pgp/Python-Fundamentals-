023fd4bdb63b00e9ef2df38e7bcffb2aa2ed0b0d
Dean Svahnalam <56052788+Cyber-pgp@users.noreply.github.com>
2024-04-01 23:25:41 +0200

    Update string_indexting.py
    describing script indexting

--- string_indexting.py
+++ string_indexting.py
@@ -1,1 +1,18 @@
+# String indexing is like picking a specific Lego block from a set knowing its position. 
+# For example, in the word "CAKE", 'C' is at position 0, 'A' is at 1, 'K' is at 2, and 'E' is at 3.
+# We use these positions to grab any letter we want.
 
+word = "CAKE"
+print(word[0] # This will print 'C'
+print(word[3] # This will print 'E'
+# There's a cool trick! We can use negative numbers to start from the end.
+print([-1] # This will print 'E'
+
+# By using position numbers we can pick out any letters from the word.
+
+# Lab 1
+# Given the string | s = 'Python' | Use indexning to print the following output:'o''Pyth''yth''nohtyP'
+
+s = 'Python'
+print(s[4] # This will print 'o'
+print(s[
