f1ec7f5198a68c33d60c96242cea75caab33a612
Dean Svahnalam <56052788+Cyber-pgp@users.noreply.github.com>
2024-04-02 22:20:24 +0200

    Update Accessing_Dictionary_Values.py

--- Accessing_Dictionary_Values.py
+++ Accessing_Dictionary_Values.py
@@ -1,1 +1,4 @@
-
+# Accessing Dictionay
+# Imagine you have a magical book that tells you where all your toys are. You just need to say the name of the toy, and the book tells you exactly where it is. 
+# This magical book is like a dictionary in our computer world. Each toy's name is a "key," and where it tells you the toy is, that's the "value."
+# For example, if you have a toy car and it's under your bed, the book (or dictionary) would look something like this in our computer language:
